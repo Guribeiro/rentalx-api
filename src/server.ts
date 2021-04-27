aa292ca4a1049be7bf21f4e5a831098c01f87ee2
Gustavo Ribeiro <gugahribeiro@hotmail.com>
2021-04-27 12:32:36 -0300

    chore: add routes

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import express from 'express';
 
+import routes from './routes';
+
 const app = express();
 
 app.use(express.json());
 
+app.use(routes);
+
 app.listen(3333, () => {
-    console.log('server is running at http://localhost:3333')
-})
+  console.log('server is running at http://localhost:3333');
+});
