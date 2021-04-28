e7937aceccac1249f40de456d7ff004b9475d295
Gustavo Ribeiro <gugahribeiro@hotmail.com>
2021-04-27 22:09:01 -0300

    chore: add models

---
+++ src/modules/vehicles/models/Specification.ts
@@ -0,0 +1,17 @@
+import { v4 as uuidV4 } from 'uuid';
+
+class Specification {
+  id: string;
+  name: string;
+  description: string;
+  created_at: Date;
+
+  constructor({ name, description, created_at }: Omit<Specification, 'id'>) {
+    this.id = uuidV4();
+    this.name = name;
+    this.description = description;
+    this.created_at = created_at;
+  }
+}
+
+export { Specification };
