ba186c3a17300bb4302d664d03788d46708b9b8f
Gustavo Ribeiro <gugahribeiro@hotmail.com>
2021-04-27 22:02:26 -0300

    chore: add vehicles dtos

---
+++ src/modules/vehicles/dtos/ICreateCategoryDTO.ts
@@ -0,0 +1,7 @@
+interface ICreateCategoryDTO {
+  name: string;
+  description: string;
+  created_at: Date;
+}
+
+export { ICreateCategoryDTO };
---
+++ src/modules/vehicles/dtos/ICreateSpecificationDTO.ts
@@ -0,0 +1,7 @@
+interface ICreateSpecificationDTO {
+  name: string;
+  description: string;
+  created_at: Date;
+}
+
+export { ICreateSpecificationDTO };
