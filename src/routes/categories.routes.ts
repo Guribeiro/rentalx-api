8eaa3a72277b1ed15596f698b8aa2638bd63cec3
Gustavo Ribeiro <gugahribeiro@hotmail.com>
2021-04-27 22:08:42 -0300

    chore: add routes

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,32 +1,16 @@
 import { Router } from 'express';
 
-import { CategoryRepository } from '../repositories/CategoryRepository';
-import { CreateCategoryService } from '../services/CreateCategoryService';
+import { createCategoryController } from '../modules/vehicles/useCases/CreateCategory';
+import { listCategoriesController } from '../modules/vehicles/useCases/ListCategories';
 
 const categoriesRoutes = Router();
-const categoryRepository = new CategoryRepository();
 
 categoriesRoutes.post('/', (request, response) => {
-  try {
-    const { name, description } = request.body;
-
-    const createCategoryService = new CreateCategoryService(categoryRepository);
-
-    const category = createCategoryService.execute({
-      name,
-      description,
-    });
-
-    return response.status(200).json(category);
-  } catch (error) {
-    return response.status(400).json({ error: error.message });
-  }
+  return createCategoryController.handle(request, response);
 });
 
 categoriesRoutes.get('/', (request, response) => {
-  const categories = categoryRepository.index();
-
-  return response.status(200).json(categories);
+  return listCategoriesController.handle(request, response);
 });
 
 export { categoriesRoutes };
